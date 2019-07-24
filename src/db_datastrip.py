51802791b0a67d5f712711bceb8c5b64810bf67a
saurex83 <pvp@dcconsult.ru>
2019-07-24 18:31:50 +0300

    Stabel version

--- src/db_datastrip.py
+++ src/db_datastrip.py
@@ -22,7 +22,7 @@ DB_Session = sessionmaker(bind = DB_Engine)
 TMIN = 60
 THOUR = 60*TMIN
 TDAY = 24*THOUR
-STRIP_INTERVAL = 100*TDAY 		#сек
+STRIP_INTERVAL = 10*TDAY 		#сек
 
 class DBStriper(threading.Thread):
 	""" """
@@ -42,26 +42,31 @@ class DBStriper(threading.Thread):
 
 	def _strip_CURDATA(self):
 		session = DB_Session()
+
 		current_time = datetime.datetime.now()
 		last_time = current_time - datetime.timedelta(seconds=STRIP_INTERVAL)
 		
-		deleted = session.query(
-			AverageCurr.value).filter(
-		AverageCurr.time < last_time).delete()
+		deleted = session.query(Curdata)
+		deleted = deleted.filter(Curdata.time < last_time)
+		deleted = deleted.delete()
 
 		logger.warning('Из таблицы CURDATA удалено %i записей'%(deleted))
 		session.commit()
 		session.close()
 
-
-class AverageCurr(DB_Base):
+# Таблица измеренных данных
+class Curdata(DB_Base):
 	__tablename__ = 'CURDATA'
 	id = Column(Integer, primary_key = True)
 	time = Column(DateTime(timezone=True), default=datetime.datetime.now)
+	# DateTime(timezone=True), server_default=func.now()
+	# time = Column(DateTime, default=datetime.datetime.now)
 	ch = Column(Integer)
-	value = Column(Float)
+	c_avr = Column(Float)
+	c_max = Column(Float)
 
-	def __init__(self, value, ch):
-		self.value = value
+	def __init__(self, ch, cavr, cmax):
+		self.c_avr = cavr
+		self.c_max = cmax
 		self.ch = ch
 
